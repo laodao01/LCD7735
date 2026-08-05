92da9d5a83340d5696f4e88da391ffabfbf25aaf
laodao01 <64339485+laodao01@users.noreply.github.com>
2026-08-06 00:34:23 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1480,16 +1480,16 @@ namespace LCD7735 {
         //set the X coordinates
         LCD_WriteReg(0x2A);
         LCD_WriteData_8Bit(0x00);
-        LCD_WriteData_8Bit(0x0);
+        LCD_WriteData_8Bit(0x02);
         LCD_WriteData_8Bit(0x00);
-        LCD_WriteData_8Bit(0x9f);
+        LCD_WriteData_8Bit(0xa1);
 
         //set the Y coordinates
         LCD_WriteReg(0x2B);
         LCD_WriteData_8Bit(0x00);
+        LCD_WriteData_8Bit(0x02);
         LCD_WriteData_8Bit(0x00);
-        LCD_WriteData_8Bit(0x00);
-        LCD_WriteData_8Bit(0x7f);
+        LCD_WriteData_8Bit(0x81);
 
         LCD_WriteReg(0x2C);
     }
